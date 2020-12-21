d060f8a65afcd3169400933ea13b4cc8432a741d
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-12-21 14:13:53 -0500

    [Update]First Update

---
+++ 0x02-unity-scripting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1000f;
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w") || Input.GetKey("up"))
+        {
+            rigidbody.AddForce(0, 0, speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+            rigidbody.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rigidbody.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rigidbody.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
