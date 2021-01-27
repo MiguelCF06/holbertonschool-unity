a9470ee07c177a8d113116d03203ff65d9480f9a
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-01-27 16:02:47 -0500

    [Updated]Added the finish line

--- 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public Text timerText;
+    bool finish = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,21 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (finish)
+            return;
+
         float time = Time.time;
 
         string minutes = ((int) time / 60).ToString();
         string seconds = (time % 60).ToString("00.00");
 
         timerText.text = minutes + ":" + seconds;
+    }
+
+    public void WinTimer()
+    {
+        finish = true;
+        timerText.color = Color.green;
+        timerText.fontSize = 60;
     }
 }
