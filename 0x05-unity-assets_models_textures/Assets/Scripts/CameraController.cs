d1380aff5fb38d1527ba46b9344f473af3a10fd0
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-01-27 14:52:18 -0500

    [Updated]Added camera controller

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerCameraParent;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 60.0f;
+    Vector2 rotation = Vector2.zero;
+
+    void Start()
+    {
+        rotation.y = transform.eulerAngles.y;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();   
+    }
+
+    void CameraRotation()
+    {
+        rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+        rotation.x -= Input.GetAxis("Mouse Y") * lookSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+        playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+        transform.eulerAngles = new Vector2(0, rotation.y);
+    }
+}
--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -39,5 +39,4 @@ public class PlayerController : MonoBehaviour
     controller.Move(playerVelocity * Time.deltaTime);
 
   }
-
 }
