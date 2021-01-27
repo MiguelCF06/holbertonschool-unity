7d60b97ed407b93c2226619330b1e77b415a6fad
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-01-27 15:51:36 -0500

    [Updated]Timer is working

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time;
+
+        string minutes = ((int) time / 60).ToString();
+        string seconds = (time % 60).ToString("00.00");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+}
