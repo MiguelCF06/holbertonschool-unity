26cf85040669b7be08b2958d26d465e60b6e4b35
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-01-27 13:39:07 -0500

    [Updated]Added player controller

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public CharacterController controller;
+  private bool groundedPlayer;
+  private Vector3 playerVelocity;
+  public float playerSpeed = 6.0f;
+  public float verticalSpeed = 0;
+  public float jumpHeight = 1.5f;
+  public float gravityValue = -9.81f;
+
+  void Update()
+  {
+    Move();
+  }
+
+  void Move()
+  {
+    float horizontalMove = Input.GetAxis("Horizontal");
+    float verticalMove = Input.GetAxis("Vertical");
+    groundedPlayer = controller.isGrounded;
+
+    if (groundedPlayer && playerVelocity.y < 0)
+    {
+      playerVelocity.y = 0f;
+    }
+
+    Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
+    controller.Move(playerSpeed * Time.deltaTime * move);
+
+
+    if (Input.GetButtonDown("Jump") && groundedPlayer)
+      playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+
+    playerVelocity.y += gravityValue * Time.deltaTime;
+    controller.Move(playerVelocity * Time.deltaTime);
+
+  }
+
+}
