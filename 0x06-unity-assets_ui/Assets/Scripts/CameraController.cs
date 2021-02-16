08e6aaf0d2cd0d07ab7968fbc9187b0637cfc448
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-02-16 10:19:40 -0500

    [Update]First Update

---
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerCameraParent;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 60.0f;
+    Vector2 rotation = Vector2.zero;
+
+    void Start()
+    {
+        rotation.y = transform.eulerAngles.y;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();   
+    }
+
+    void CameraRotation()
+    {
+        float horizontalRotationPlayer = Input.GetAxis("Mouse X") * lookSpeed;
+        rotation.y = Input.GetAxis("Mouse X") * lookSpeed;
+        rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+
+        float y = Mathf.Round(transform.eulerAngles.y * lookSpeed);
+        Vector3 cameraEulerAngles = new Vector3(0, y, 0);
+
+        playerCameraParent.parent.eulerAngles = cameraEulerAngles;
+        playerCameraParent.localRotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+        
+        transform.eulerAngles = new Vector2(0, rotation.y);
+        transform.LookAt(playerCameraParent.parent.transform);
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    bool finish = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (finish)
+            return;
+
+        float time = Time.time;
+
+        string minutes = ((int) time / 60).ToString();
+        string seconds = (time % 60).ToString("00.00");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+
+    public void WinTimer()
+    {
+        finish = true;
+        timerText.color = Color.green;
+        timerText.fontSize = 60;
+    }
+}
