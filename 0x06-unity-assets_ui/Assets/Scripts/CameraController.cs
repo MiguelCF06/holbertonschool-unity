ac8737ae001c16ca77cfc11a3d331d9f1cfe916f
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-02-18 09:16:07 -0500

    [Update]Added the option to invert the y axis of the camera

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour
     public float lookSpeed = 2.0f;
     public float lookXLimit = 60.0f;
     Vector2 rotation = Vector2.zero;
+    public bool isInverted;
 
     void Start()
     {
@@ -25,7 +26,14 @@ public class CameraController : MonoBehaviour
     {
         float horizontalRotationPlayer = Input.GetAxis("Mouse X") * lookSpeed;
         rotation.y = Input.GetAxis("Mouse X") * lookSpeed;
-        rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+        if (!isInverted)
+        {
+            rotation.x -= -Input.GetAxis("Mouse Y") * lookSpeed;
+        }
+        else
+        {
+            rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+        }
         rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
 
         float y = Mathf.Round(transform.eulerAngles.y * lookSpeed);
