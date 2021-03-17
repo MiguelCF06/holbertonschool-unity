89edba8787df4ef2b59422028bff4563d1e06f9d
MiguelCF06 <miguel.cipamocha@gmail.com>
2021-03-17 08:54:15 -0500

    [Updated]BGM become muffled when paused

--- 0x08-unity-audio/Assets/Scripts/PauseMenu.cs
+++ 0x08-unity-audio/Assets/Scripts/PauseMenu.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class PauseMenu : MonoBehaviour
 {
       [SerializeField] GameObject pauseCanvas;
       [SerializeField] Timer time;
+      [SerializeField] AudioMixerSnapshot paused;
+      [SerializeField] AudioMixerSnapshot unpaused;
       bool isPaused;
 
       void Update()
@@ -30,33 +33,47 @@ public class PauseMenu : MonoBehaviour
             isPaused = true;
             pauseCanvas.SetActive(true);
             Time.timeScale = 0f;
+            Lowpass();
       }
 
       public void Resume()
       {
-          Cursor.lockState = CursorLockMode.Locked;
-          pauseCanvas.SetActive(false);
-          Time.timeScale = 1f;
-          isPaused = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            pauseCanvas.SetActive(false);
+            Time.timeScale = 1f;
+            isPaused = false;
+            Lowpass();
       }
 
       public void Restart()
       {
-          time.time = 0.0f;
-          Time.timeScale = 1f;
-          SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            time.time = 0.0f;
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
       }
 
       public void MainMenu()
       {
-          Time.timeScale = 1f;
-          SceneManager.LoadScene("MainMenu");
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenu");
       }
 
       public void Options()
       {
-          Time.timeScale = 1f;
-          PlayerPrefs.SetString("lastScene", SceneManager.GetActiveScene().name);
-          SceneManager.LoadScene("Options");
+            Time.timeScale = 1f;
+            PlayerPrefs.SetString("lastScene", SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene("Options");
+      }
+
+      void Lowpass()
+      {
+            if (Time.timeScale == 0)
+            {
+                paused.TransitionTo(.01f);
+            }
+            else
+            {
+                unpaused.TransitionTo(.01f);
+            }
       }
 }
